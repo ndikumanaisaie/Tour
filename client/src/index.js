dc39dfa9d9986b7ff75f10a9635ffcf321075458
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-09-10 21:51:11 +0100

    Setting up MDB

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import App from './App.js';
 import postsReducer from './features/posts/postsSlice.js';
 

d25b28e2de212fa37b8015c1232b7a341fa2526f
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-09-10 22:37:04 +0100

    Initial setup of  files and folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,30 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-	Container, Grow, Typography, AppBar, Grid, Toolbar,
-} from '@mui/material';
+// import { ToastContainer } from 'react-toastify';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Form from './components/Form/Form.jsx';
-import Posts from './components/Posts/Posts.jsx';
-import { getPosts } from './features/posts/postsSlice.js';
 
-import memories from './images/memories.png';
-
-import './styles.css';
+import './app.css'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
-	const dispatch = useDispatch();
+	// const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(getPosts());
-	}, [dispatch]);
+	// useEffect(() => {
+	// 	dispatch(getPosts());
+	// }, [dispatch]);
 
 	return (
-		<Container maxWidth="lg">
-			<AppBar position="static" className="menu-bar" color="inherit">
-				<Toolbar variant="dense">
-					<Typography variant="h3" color="rgba(0,185,225, 1)" component="div">
-						Memories
-					</Typography>
-					<img src={memories} alt="memories" height={30} className="logo" />
-				</Toolbar>
-			</AppBar>
-			<Grow in>
-				<Container>
-					<Grid container justifyContent="space-between" alignItems="stretch" spacing={3} >
-						<Grid item xs={12} sm={7}>
-							<Posts />
-						</Grid>
-						<Grid item xs={12} sm={4}>
-							<Form />
-						</Grid>
-					</Grid>
-				</Container>
-			</Grow>
-		</Container>
+		<div className='app'>
+				<Routes>
+					<Route path='/' element={ <Home />} />
+					<Route path='/login' element={ <Login />} />
+					<Route path='/register' element={ <Register />} />
+				</Routes>
+		</div>
 	);
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import './index.css'
 import App from './App.js';
 import postsReducer from './features/posts/postsSlice.js';
 

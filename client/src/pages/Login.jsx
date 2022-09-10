53097429fb234f757b1bbdeb2398c18f13c3e1d4
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-09-10 23:51:50 +0100

    Implement login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react';
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCardFooter,
+  MDBValidation,
+  MDBBtn,
+  MDBIcon,
+  MDBSpinner,
+} from 'mdb-react-ui-kit';
+import { Link } from 'react-router-dom';
 
+const initialState = {
+  email: '',
+  password: '',
+}
 const Login = () => {
+  const [formValue, setFormValue] = useState(initialState);
+  const { email, password } = formValue;
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  };
+  const onInputChange = (e) => {
+    let { name, value } = e.target;
+    setFormValue({...formValue, [name]: value });
+  };
+
   return (
-    <div>Login</div>
+    <div
+      style={{
+        margin: 'auto',
+        padding: '15px',
+        maxWidth: '450px',
+        alignContent: 'center',
+        marginTop: '120px',
+      }}
+    >
+      <MDBCard>
+        <MDBIcon fas icon='user-circle' className='fa-2x'/>
+        <h5>Sign In</h5>
+        <MDBCardBody>
+          <MDBValidation onSubmit={handleSubmit} noValidate className='row g-3'> 
+            <div className='col-md-12'>
+              <MDBInput 
+                label='email'
+                type='email'
+                value={email}
+                onChange={onInputChange}
+                name='email'
+                required
+                validation="Please provide your email"
+              />
+            </div>
+            <div className='col-md-12'>
+              <MDBInput 
+                label='password'
+                type='password'
+                value={password}
+                onChange={onInputChange}
+                name='password'
+                required
+                validation="Please provide your password"
+              />
+            </div>
+            <div className='col-12'>
+              <MDBBtn style={{ width: '100%'}} className='mt-2'>
+                Login
+              </MDBBtn>
+            </div>
+          </MDBValidation>
+        </MDBCardBody>
+        <MDBCardFooter>
+          <Link to='/Register'>
+            <p>Don't have an account? Sign up</p>
+          </Link>
+        </MDBCardFooter>
+      </MDBCard>
+    </div>
   )
 }
 

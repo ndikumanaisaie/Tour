44ae1014937f233c54f42caaadc7d621711f1ea6
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-09-22 10:37:09 +0100

    Add singlepost logic

--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   MDBCard, 
   MDBCardBody,
@@ -11,10 +11,56 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import moment from 'moment';
+import { getPost } from '../features/postSlice';
 
 const SinglePost = () => {
+  const dispatch = useDispatch();
+  const { post } = useSelector((state) => ({ ...state.posts}));
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      dispatch(getPost(id));
+    }
+  }, [id])
+  
+
   return (
-    <div>SinglePost</div>
+    <MDBContainer>
+      <MDBCard className='mb-3 mt-2'>
+        <MDBCardImage 
+          position='top'
+          style={{ widith: '100%', maxHeight: '600px' }}
+          src={post.imageFile}
+          alt={post.title}
+        />
+        <MDBCardBody>
+          <h3>{post?.title}</h3>
+          <span>
+            <p className='text-start post-name'>Created by: {post.name}</p>
+          </span>
+          <div style={{ float: 'left' }}>
+            <span className='text-start'>
+              {
+                post && post.tags && tags.map((tag) => `#${post} `)
+              }
+            </span>
+          </div>
+          <br />
+          <MDBCardText className='text-start mt-2'>
+            <MDBIcon 
+              style={{ float: 'left', margin: '5px' }}
+              far
+              icon='calendar-alt'
+              size='lg'
+            />
+            <small className='text-muted'>
+              {moment(post.createdAt).fromNow()}
+            </small>
+          </MDBCardText>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBContainer>
   )
 }
 
